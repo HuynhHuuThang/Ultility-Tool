106d881892cd169b351efd8f8eb96a742899b8d3
Thang <21521771@gm.uit.edu.vn>
2024-08-01 13:18:35 +0700

    save log data for visualizing down up ping

--- network_perfomance_log_saver.py
+++ network_perfomance_log_saver.py
@@ -8,7 +8,8 @@ log_filename=f'wifi_speed_{timestamp}.log'
 logging.basicConfig(filename=log_filename, level=logging.INFO, 
                     format='%(asctime)s - %(message)s')
 
-def log_wifi_speed():
+def log_wifi_speed(fname):
+    filename=fname
     # Create a Speedtest object
     st = speedtest.Speedtest(secure=True)
     # Get best server based on ping
@@ -19,7 +20,7 @@ def log_wifi_speed():
     upload_speed = st.upload() / 1_000_000  # Convert to Mbps
     ping = st.results.ping
     # Save data to a text file
-    with open('data.txt', 'a') as file:
+    with open(f'data_{filename}.txt', 'a') as file:
         file.write(f'Download speed: {download_speed:.2f} Mbps\n')
         file.write(f'Upload speed: {upload_speed:.2f} Mbps\n')
         file.write(f'Ping: {ping:.2f} ms\n')
@@ -27,10 +28,51 @@ def log_wifi_speed():
     logging.info(f'Download speed: {download_speed:.2f} Mbps')
     logging.info(f'Upload speed: {upload_speed:.2f} Mbps')
     logging.info(f'Ping: {ping:.2f} ms')
-
+def log_down_up_ping_speed(location, dname, upname ,pingname , download_folder_path, upload_folder_path, ping_folder_path):
+    download_filename=dname
+    upload_filename=upname
+    ping_filename=pingname
+    location_name=location
+    # Create a Speedtest object
+    st = speedtest.Speedtest(secure=True)
+    # Get best server based on ping
+    st.get_best_server()
+    
+    # Perform download speed test
+    download_speed = st.download() / 1_000_000  # Convert to Mbps
+    # Perform upload speed test
+    upload_speed = st.upload() / 1_000_000 
+    #Perform ping test
+    ping = st.results.ping
+    # Save data to a text file
+    with open(f'{download_folder_path}/data_{download_filename}.txt', 'a') as file:
+        file.write(f'Location: {location_name}\n')
+        file.write(f'Download speed: {download_speed:.2f} Mbps\n')
+        file.write("-" * 30 + "\n")
+    with open(f'{upload_folder_path}/data_{upload_filename}.txt', 'a') as file:
+        file.write(f'Location: {location_name}\n')
+        file.write(f'Upload speed: {upload_speed:.2f} Mbps\n')
+        file.write("-" * 30 + "\n")
+    with open(f'{ping_folder_path}/data_{ping_filename}.txt', 'a') as file:
+        file.write(f'Location: {location_name}\n')
+        file.write(f'Ping: {ping:.2f} ms\n')
+        file.write("-" * 30 + "\n")
 if __name__ == "__main__":
     location_name = input("Enter the location name: ")
+    filename = location_name + '_wifi_speed.txt'
+    # Specify the folder path
+    download_log_folder_path = './log/download'
+    upload_log_folder_path = './log/upload'
+    ping_log_folder_path = './log/ping'
+    #specify the log file names
+    download_log_name = location_name + '_download_speed.txt'
+    upload_log_name = location_name + '_upload_speed.txt'
+    ping_log_name = location_name + '_ping_speed.txt'
     logging.info(f'Location: {location_name}')
-    with open('data.txt', 'a') as file:
+    with open(f'{ping_log_folder_path}/data_{filename}', 'a') as file:
         file.write(f'Location: {location_name}\n')
-    log_wifi_speed()
+    with open(f'data_{filename}.txt', 'a') as file:
+        file.write(f'Location: {location_name}\n')
+    log_wifi_speed(filename)
+    log_down_up_ping_speed(location_name, download_log_name, upload_log_name, ping_log_name, download_log_folder_path, upload_log_folder_path, ping_log_folder_path)
+  
--- wifi_identifier.py
+++ wifi_identifier.py
@@ -1,31 +1,29 @@
 import pywifi
 from pywifi import const
 import time
-import logging
 
-def remove_all_profiles(iface):
-    profiles = iface.network_profiles()
-    for profile in profiles:
-        iface.remove_network_profile(profile)
 
-def get_wifi_stats():
-
-    start_time = time.time()
-    duration=60
+def get_nearby_wifi_stats():
 
     wifi = pywifi.PyWiFi()
     iface = wifi.interfaces()[0]
-    while time.time() - start_time < duration:
-        iface.scan()
-        scan_results = iface.scan_results()
-        for data in scan_results:
-            print(f"SSID: {data.ssid}")
-            print(f"BSSID: {data.bssid}")
-            print(f"{data.ssid} Signal Strength: {data.signal}")
-            print(f"Frequency: {data.freq} MHz")
-    print("\n" + "=" * 50 + "\n")
-    # Wait for 5 seconds before the next scan
-    time.sleep(5)  
+    iface.scan()
+    scan_results = iface.scan_results()
+    for data in scan_results:
+        # print(f"SSID: {data.ssid}")
+        # print(f"BSSID: {data.bssid}")
+        # print(f"{data.ssid} Signal Strength: {data.signal}")
+        # print(f"Frequency: {data.freq} MHz")
+        # Save data to a text file
+        with open("wifi_nearby_stats.txt", "a") as file:
+            for data in scan_results:
+                file.write(f"SSID: {data.ssid}\n")
+                file.write(f"BSSID: {data.bssid}\n")
+                file.write(f"{data.ssid} Signal Strength: {data.signal}\n")
+                file.write(f"Frequency: {data.freq} MHz\n")
+                file.write("-" * 30 + "\n")
+            break
+   
 
 def get_connected_wifi_ssid():
     wifi = pywifi.PyWiFi()
@@ -37,19 +35,24 @@ def get_connected_wifi_ssid():
     
     for network in scan_results:
         if network.ssid == connected_ssid and iface.status() == const.IFACE_CONNECTED:
-            print(f"Connected to: {network.ssid}")
-            print(f"Signal Strength: {network.signal} dbm")
-            print(f"Frequency: {network.freq} Mhz")
-            print("-" * 30)
+            with open("wifi_nearby_stats.txt", "a") as file:
+                file.write(f"Connected to: {network.ssid}\n")
+                file.write(f"BSSID: {network.bssid}\n")
+                file.write(f"{network.ssid} Signal Strength: {network.signal}\n")
+                file.write(f"Frequency: {network.freq} MHz\n")
+                file.write("-" * 30 + "\n")
+            # print(f"Connected to: {network.ssid}")
+            # print(f"BSSID: {network.bssid}")
+            # print(f"Signal Strength: {network.signal} dbm")
+            # print(f"Frequency: {network.freq} Mhz")
+            # print("-" * 30)
             break
     else:
         print("No connected WiFi network found.")
 
 if __name__ == "__main__":
     get_connected_wifi_ssid()
-
-
-    # get_wifi_stats()
+    get_nearby_wifi_stats()
 
 
         # Get the connected WiFi network
